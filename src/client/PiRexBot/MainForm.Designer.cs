33902882af86d0d8b3c90ae76b78d5e4477d3930
Andrew Kirillov <andrew.kirillov@gmail.com>
2018-02-22 23:24:56 +0000

    Adding initial skeleton of a .NET client application to control PiRex Bot.

---
+++ src/client/PiRexBot/MainForm.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace PiRexBot
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( )
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ipAddressLabel = new System.Windows.Forms.Label();
+            this.ipAddressBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.portUpDown = new System.Windows.Forms.NumericUpDown();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
+            this.menuStrip.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.portUpDown)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(732, 28);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // fileMenuItem
+            // 
+            this.fileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitMenuItem});
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileMenuItem.Text = "&File";
+            // 
+            // exitMenuItem
+            // 
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Size = new System.Drawing.Size(102, 24);
+            this.exitMenuItem.Text = "E&xit";
+            this.exitMenuItem.Click += new System.EventHandler(this.exitMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 636);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(732, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.disconnectButton);
+            this.groupBox1.Controls.Add(this.connectButton);
+            this.groupBox1.Controls.Add(this.portUpDown);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.ipAddressBox);
+            this.groupBox1.Controls.Add(this.ipAddressLabel);
+            this.groupBox1.Location = new System.Drawing.Point(10, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(700, 66);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection";
+            // 
+            // ipAddressLabel
+            // 
+            this.ipAddressLabel.AutoSize = true;
+            this.ipAddressLabel.Location = new System.Drawing.Point(10, 28);
+            this.ipAddressLabel.Name = "ipAddressLabel";
+            this.ipAddressLabel.Size = new System.Drawing.Size(80, 17);
+            this.ipAddressLabel.TabIndex = 0;
+            this.ipAddressLabel.Text = "IP Address:";
+            // 
+            // ipAddressBox
+            // 
+            this.ipAddressBox.Location = new System.Drawing.Point(95, 25);
+            this.ipAddressBox.Name = "ipAddressBox";
+            this.ipAddressBox.Size = new System.Drawing.Size(150, 22);
+            this.ipAddressBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(260, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Port:";
+            // 
+            // portUpDown
+            // 
+            this.portUpDown.Location = new System.Drawing.Point(305, 25);
+            this.portUpDown.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.portUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.portUpDown.Name = "portUpDown";
+            this.portUpDown.Size = new System.Drawing.Size(120, 22);
+            this.portUpDown.TabIndex = 3;
+            this.portUpDown.Value = new decimal(new int[] {
+            8000,
+            0,
+            0,
+            0});
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(440, 24);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(90, 24);
+            this.connectButton.TabIndex = 4;
+            this.connectButton.Text = "&Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Location = new System.Drawing.Point(535, 24);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(90, 24);
+            this.disconnectButton.TabIndex = 5;
+            this.disconnectButton.Text = "&Disconnect";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.videoSourcePlayer);
+            this.groupBox2.Location = new System.Drawing.Point(10, 110);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(700, 517);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Camera View";
+            // 
+            // videoSourcePlayer
+            // 
+            this.videoSourcePlayer.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.videoSourcePlayer.Location = new System.Drawing.Point(15, 25);
+            this.videoSourcePlayer.Name = "videoSourcePlayer";
+            this.videoSourcePlayer.Size = new System.Drawing.Size(642, 482);
+            this.videoSourcePlayer.TabIndex = 0;
+            this.videoSourcePlayer.VideoSource = null;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 658);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PiRex Bot";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.portUpDown)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.NumericUpDown portUpDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ipAddressBox;
+        private System.Windows.Forms.Label ipAddressLabel;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
+    }
+}
+
---
+++ src/client/PiRexBot/MainForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AForge.Video;
+
+namespace PiRexBot
+{
+    public partial class MainForm : Form
+    {
+        public MainForm( )
+        {
+            InitializeComponent( );
+        }
+
+        private void MainForm_FormClosing( object sender, FormClosingEventArgs e )
+        {
+            Disconnect( );
+        }
+
+        private void exitMenuItem_Click( object sender, EventArgs e )
+        {
+            this.Close( );
+        }
+
+        private void Disconnect( )
+        {
+            videoSourcePlayer.Stop( );
+            videoSourcePlayer.VideoSource = null;
+        }
+
+        private void connectButton_Click( object sender, EventArgs e )
+        {
+            MJPEGStream videoSource = new MJPEGStream( string.Format( "http://{0}:{1}/camera/mjpeg",
+                                                       ipAddressBox.Text.Trim( ), portUpDown.Value ) );
+
+            videoSourcePlayer.VideoSource = videoSource;
+            videoSourcePlayer.Start( );
+        }
+
+        private void disconnectButton_Click( object sender, EventArgs e )
+        {
+            Disconnect( );
+        }
+
+
+    }
+}
